a24fe080eed86064510aa6c5698fadea43635ae7
RositsaMiteva <113831422+RositsaMiteva@users.noreply.github.com>
2023-01-27 21:51:20 +0100

    Add project files.

---
+++ Collection.UnitTests/CollectionTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Collection.UnitTests
+{
+    public class CollectionTests
+    {
+       
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
