476eb153feb8753c080b8e6f65390758cc57bb66
RositsaMiteva <113831422+RositsaMiteva@users.noreply.github.com>
2023-01-28 13:17:23 +0100

    Adding tests

--- Collection.UnitTests/CollectionTests.cs
+++ Collection.UnitTests/CollectionTests.cs
@@ -1,3 +1,6 @@
+
+
+using Collections;
 using NUnit.Framework;
 
 namespace Collection.UnitTests
@@ -6,9 +9,53 @@ namespace Collection.UnitTests
     {
        
         [Test]
-        public void Test1()
+
+        public void Test_Collection_EmptyConstructor() 
         {
-            Assert.Pass();
+            //Arrange and Act
+            var coll = new Collection<int>();
+            Assert.AreEqual(coll.ToString(), "[]");
         }
+        [Test]
+        public void Test_Collection_ConstructorSingleItem() 
+        {
+            var coll = new Collection<int>(5);
+            //Assert
+            Assert.AreEqual(coll.ToString(), "[5]");
+        }
+        [Test]
+        public void Test_Collection_ConstructorMultipleItems() 
+        {
+            var coll = new Collection<int>(2, 2);
+            //Assert
+            Assert.AreEqual(coll.ToString(), "[2, 2]");
+        }
+        [Test]
+        public void Test_Collection_Add() 
+        {
+            //Arrange
+            var coll = new Collection<int>();
+            //Act
+            coll.Add(5);
+            coll.Add(6);
+            //Assert
+            Assert.That(coll.ToString(), Is.EqualTo("[5, 6]"));
+        }
+        [Test]
+        public void Test_Collection_AddRangeWithGrow() 
+        {
+            //Arrange
+            var coll = new Collection<int>();
+            //Act
+            coll.AddRange(5 + 5);
+            //Assert
+            Assert.That(coll.ToString(), Is.EqualTo("[10]"));
+
+        }
+
+
+
+
+
     }
 }
